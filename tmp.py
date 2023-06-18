b1b9882dc0edc940b4bd6f2d984a81d0aaa70dd5
Summer Dupree <127756170+sdupree27886@users.noreply.github.com>
2023-06-18 20:01:39 +0000

    inserted lat and lon, changed popup description

--- tmp.py
+++ tmp.py
@@ -1,21 +1,11 @@
-# import folium
-# from io import BytesIO
-# import matplotlib.pyplot as plt
-# import matplotlib.dates as dates
-# import os
-# import pathlib
-# import requests
-# import subprocess
-
-# BEGIN SOLUTION
+# standard library packages
 import os
 import pathlib
 import subprocess
 from io import BytesIO
 
 import folium
-import matplotlib.pyplot as plt
 import matplotlib.dates as dates
+import matplotlib.pyplot as plt
 import pandas as pd
 import requests
-# END SOLUTION
